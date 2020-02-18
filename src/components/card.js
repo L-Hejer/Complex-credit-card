cebbb1c830edcc6c699d81de5f27d12ab19ba3f0
L-Hejer <laouani.h@gmail.com>
2020-02-18 09:37:58 +0100

    date change

--- src/components/card.js
+++ src/components/card.js
@@ -1,37 +1,38 @@
-import React from "react";
-import logo from "./mastercard-icon.jpg";
-import chip from "./chip.png";
+import React from 'react';
+
+import logo from './mastercard-icon.jpg';
+import chip from './chip.png';
 
 class Card extends React.Component {
   state = {
-    name: "NOAH GRAHAM",
+    name: 'NOAH GRAHAM',
     number: [
-      "*",
-      "*",
-      "*",
-      "*",
-      " ",
-      "*",
-      "*",
-      "*",
-      "*",
-      " ",
-      "*",
-      "*",
-      "*",
-      "*",
-      " ",
-      "*",
-      "*",
-      "*",
-      "*"
+      '*',
+      '*',
+      '*',
+      '*',
+      ' ',
+      '*',
+      '*',
+      '*',
+      '*',
+      ' ',
+      '*',
+      '*',
+      '*',
+      '*',
+      ' ',
+      '*',
+      '*',
+      '*',
+      '*'
     ],
-    valid: ["*", "*", "/", "*", "*"]
+    valid: ['*', '*', '/', '*', '*']
   };
 
   //Name of Card Owner
   nameChange = e => {
-    e.target.value = e.target.value.replace(/[^a-zA-Z ]/g, "");
+    e.target.value = e.target.value.replace(/[^a-zA-Z ]/g, '');
     this.setState({
       name: e.target.value.toUpperCase()
     });
@@ -40,15 +41,15 @@ class Card extends React.Component {
   //Card Number
   numberChange = e => {
     e.target.value = e.target.value
-      .replace(/[^0-9]/g, "")
-      .replace(/(\d{4})/g, "$1 ")
+      .replace(/[^0-9]/g, '')
+      .replace(/(\d{4})/g, '$1 ')
       .trim();
     this.setState({
       number: this.state.number.map((x, i) =>
-        x === " "
-          ? " "
+        x === ' '
+          ? ' '
           : e.target.value[i] === undefined
-          ? "*"
+          ? '*'
           : e.target.value[i]
       )
     });
@@ -59,15 +60,15 @@ class Card extends React.Component {
     e.target.value =
       e.target.value.length >= 3
         ? `${e.target.value
-            .replace(/[^0-9]/g, "")
-            .slice(0, 2)}/${e.target.value.replace(/[^0-9]/g, "").slice(2, 4)}`
-        : e.target.value.replace(/[^0-9]/g, "");
+            .replace(/[^0-9]/g, '')
+            .slice(0, 2)}/${e.target.value.replace(/[^0-9]/g, '').slice(2, 4)}`
+        : e.target.value.replace(/[^0-9]/g, '');
     this.setState({
       valid: this.state.valid.map((x, i) =>
-        x === "/"
-          ? "/"
+        x === '/'
+          ? '/'
           : e.target.value[i] === undefined
-          ? "*"
+          ? '*'
           : e.target.value[i]
       )
     });
@@ -75,59 +76,52 @@ class Card extends React.Component {
 
   render() {
     return (
-      <div className="Main">
-        <div className="card-container">
+      <div className='Main'>
+        <div className='card-container'>
           <h1>CREDIT CARD</h1>
-          <img className="chip" src={chip} alt="" />
-          <div className="card-information">
+          <img className='chip' src={chip} alt='' />
+          <div className='card-information'>
             <div>
-              <p className="card-number">{this.state.number}</p>
-              <p className="card-ccv">5456</p>
-              <p className="card-name">{this.state.name}</p>
-              <div className="expiration-date">
-                <p className="validation">
+              <p className='card-number'>{this.state.number}</p>
+              <p className='card-ccv'>5456</p>
+              <p className='card-name'>{this.state.name}</p>
+              <div className='expiration-date'>
+                <p className='validation'>
                   VALID
                   <br />
                   THRU
                 </p>
-                <p className="card-date">{this.state.valid}</p>
+                <p className='card-date'>{this.state.valid}</p>
               </div>
             </div>
-            <img className="logo" src={logo} alt="" />
+            <img className='logo' src={logo} alt='' />
           </div>
         </div>
 
-        <div className="form-container">
-          <label>
-            CARD NUMBER
-            </label>
-            <input
-              type="text"
-              maxLength="19"
-              onChange={this.numberChange}
-              placeholder="**** **** **** ****"
-            />
-          
-          <label>
-            NAME
-            </label>
-            <input
-              type="text"
-              maxLength="20"
-              onChange={this.nameChange}
-              placeholder="Enter your name"
-            />
-          
-          <label>
-            EXPIRATION DATE
-            </label>
-            <input
-              type="text"
-              onChange={this.dateChange}
-              maxLength="5"
-              placeholder="MM/YY"
-            />
-          
+        <div className='form-container'>
+          <label>CARD NUMBER</label>
+          <input
+            type='text'
+            maxLength='19'
+            onChange={this.numberChange}
+            placeholder='**** **** **** ****'
+          />
+
+          <label>NAME</label>
+          <input
+            type='text'
+            maxLength='20'
+            onChange={this.nameChange}
+            placeholder='Enter your name'
+          />
+
+          <label>EXPIRATION DATE</label>
+          <input
+            type='text'
+            onChange={this.dateChange}
+            maxLength='5'
+            placeholder='MM/YY'
+          />
         </div>
       </div>
     );
